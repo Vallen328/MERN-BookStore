5e202e9e1938e5923d505785433819415f4bf07c
Vallen Dsouza <123362088+Vallen328@users.noreply.github.com>
2025-03-09 01:21:16 +0530

    Initial commit: Set up project structure with frontend and backend

---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div className="bg-zinc-800 text-white px-8 py-4">
+        <h1 className="text-xl font-semibold text-center">&copy; 2025, Made with Love</h1>
+    </div>
+  )
+}
+
+export default Footer;
