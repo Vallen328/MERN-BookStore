74f253d18137bd3ea03069631ac235d6aec246f0
Vallen Dsouza <123362088+Vallen328@users.noreply.github.com>
2025-04-27 22:11:42 +0530

    Update AllBooks.jsx

--- frontend/src/pages/AllBooks.jsx
+++ frontend/src/pages/AllBooks.jsx
@@ -7,7 +7,7 @@ const AllBooks = () => {
   const [Data, setData] = useState();
   useEffect(() => {
     const fetch = async() => {
-      const response = await axios.get("http://localhost:1000/api/v1/get-all-books");
+      const response = await axios.get("https://d5vaxs5dc6.execute-api.ap-south-1.amazonaws.com/prod/api/v1/get-all-books");
       setData(response.data.data);
     };
     fetch();
